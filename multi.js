53684aef9d9511118a0d49247e12dc95709ac534
Saulkaique <e107ti@etec.sp.gov.br>
2023-05-04 22:30:38 -0300

    Mais teste e evoluindo

--- multi.js
+++ multi.js
@@ -1,3 +1,13 @@
-const carro = 35
 
-console
+
+
+
+ 
+let  Nome =  prompt('Digite seu nome:');
+let   idade =  prompt('Digite sua idade:');
+let  Senha =  prompt('Didite sua Senha');
+let   email =  prompt('Digite E-mail:');
+
+let Usuario = nome + idade + Senha + email;
+
+console.log('$[Usuario]');
