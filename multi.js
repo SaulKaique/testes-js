d11f6b22df90d7f3339d8f7474abf9bbd83081f8
SaulKaique <Saul.silva@etec.sp.gov.br>
2023-05-10 14:58:08 -0300

    Maias teste bobos mas são testes

--- multi.js
+++ multi.js
@@ -1,13 +1,80 @@
+//let caraoucoroa = () => {
+//if (Math.random() > 0.5) {
+     
+    //return 'Coroa';
+    
+//}
+//else {
+   // return 'Cara';
+    
+
+//}
+
+
+//}//
+
+//let dados6 = () => Math.floor(Math.random() * 6) + 1 ;
+
+//let Cracks = [
+//{Nome: 'Michael Jordan', Esporte: 'Basquete'},
+//{Nome: 'Pelé', Esporte:'Fultibol'},
+//{Nome: 'Lebron James', Esporte: 'Basquete'},
+//{Nome: 'Lancres ferreira', Esporte: 'Tenis'}
+//];
+
+
+//let Ferramentas = [
+//    {Nome: 'Martelo', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado', Uso: 'Manual'},
+ //{Nome: 'Prego', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado', Uso: 'Manual'},
+ //{Nome: 'Furadera', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado', Uso: 'Eletrico'},
+ //{Nome: 'Maquita', Quantidade: 300 , qualidade: '9/10' , cor: 'Pratiado', Uso: 'Eletrico'},
+ //{Nome: 'Cerra', Quantidade: 300 , qualidade: '9/10' , cor: 'Pratiado', Uso: 'Eletrico'},
+ //{Nome: 'Chave de fenda', Quantidade: 300 , qualidade: '5/10' , cor: 'Pratiado', Uso: 'Manual'},
+ //{Nome: 'Chave inglêsa', Quantidade: 300 , qualidade: '3/10' , cor: 'Pratiado', Uso: 'Manual'},
+ //{Nome: 'Macaco', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado' , Uso: 'Manual'},
+ //{Nome: 'Marreta', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado', Uso: 'Manual'}
+//
+//
+//];
+
+//
+//let escolha = prompt('Escolha o uso eletrico ou manual ');
+//
+//let produto = Ferramentas.filter(Ferramentas => Ferramentas.Uso == `${escolha}`);
+//
+//let quadrado = x => x ** 2;
+
+
+//let numeros = [2, -5 , 0,4 , 75 , -3,5];
+//let quadrados = numeros.map(quadrado);
+//let dobros = numeros.map(n => n * 2);
+
+
+//let soma = numeros.reduce((ant, atual) => ant + atual); 
 
 
 
 
- 
-let  Nome =  prompt('Digite seu nome:');
-let   idade =  prompt('Digite sua idade:');
-let  Senha =  prompt('Didite sua Senha');
-let   email =  prompt('Digite E-mail:');
+//console.log(produto);
 
-let Usuario = nome + idade + Senha + email;
 
-console.log('$[Usuario]');
+//console.log(quadrado);
+//console.log(numeros);
+//console.log(quadrados);
+//console.log(dobros);
+//console.log(soma);
+
+
+let pessoa = {
+    nome: 'Saul',
+    dizerola2: function(){
+        console.log( `Ola meu nome é ${this.nome}`);
+
+    }
+
+
+}
+
+
+
+pessoa.dizerola2();
