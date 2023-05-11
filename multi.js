78cc9b1620cc3a8c2077f37a09f0a451e4905be3
Saul Kaique <e107ti@etec.sp.gov.br>
2023-05-10 22:36:14 -0300

    nada de mais

--- multi.js
+++ multi.js
@@ -1,80 +1,80 @@
-//let caraoucoroa = () => {
-//if (Math.random() > 0.5) {
-     
-    //return 'Coroa';
+
+let Medico
+let infermero
+let recepção
+
+
+
+let Usuario = [
+
+    {nome: 'Arthur', idade: 18 , altura: '1,96' , peso: 101, email: 'arthurgeladeradopedro@email.com' },
     
-//}
-//else {
-   // return 'Cara';
+    {nome: 'Pedro', idade: 18 , altura: '1,75' , peso: 127, email: 'Pedrocomedordeveia@email.com' },
     
+    {nome: 'Joliano', idade: 19 , altura: '1,75' , peso: 46, email: 'Jolianoburaconegro@email.com'}, 
+    
+    {nome: 'Athena', idade: 18 , altura: '1,65' , peso: 73, email: 'athenagostosa@email.com' },
+    
+    {nome: 'Nicole', idade: 18 , altura: '1,59' , peso: 80, email: 'Nicolebalão@email.com' },
+    
+    {nome: 'Big ed', idade: 18 , altura: '1,72' , peso: 128, email: 'bigedgadodemais@email.com' },
+    
+    {nome: 'Thiago', idade: 18 , altura: '1,84' , peso: 120, email: 'thigastropadoscalvo@email.com' },
+    
+    {nome: 'Rato', idade: 'sla' , altura: 'sla' , peso: 'sla', email: 'ratosla@email.com' },
+    
+    {nome: 'Ana', idade: 18 , altura: '1,60' , peso: 45, email: 'aninhacubatão@email.com' },
+    
+    {nome: 'Sara', idade: 22 , altura: '1,75' , peso: 65, email: 'Saragordafeia10kavenda@email.com' }
+    
+    
+    
+    ];
 
-//}
+    if (Usuario.nome == 'joliano') {
 
+        console.log('você é Medico esse essa e sua interface ');
 
-//}//
+         
+    } 
+    else{
 
-//let dados6 = () => Math.floor(Math.random() * 6) + 1 ;
-
-//let Cracks = [
-//{Nome: 'Michael Jordan', Esporte: 'Basquete'},
-//{Nome: 'Pelé', Esporte:'Fultibol'},
-//{Nome: 'Lebron James', Esporte: 'Basquete'},
-//{Nome: 'Lancres ferreira', Esporte: 'Tenis'}
-//];
-
-
-//let Ferramentas = [
-//    {Nome: 'Martelo', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado', Uso: 'Manual'},
- //{Nome: 'Prego', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado', Uso: 'Manual'},
- //{Nome: 'Furadera', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado', Uso: 'Eletrico'},
- //{Nome: 'Maquita', Quantidade: 300 , qualidade: '9/10' , cor: 'Pratiado', Uso: 'Eletrico'},
- //{Nome: 'Cerra', Quantidade: 300 , qualidade: '9/10' , cor: 'Pratiado', Uso: 'Eletrico'},
- //{Nome: 'Chave de fenda', Quantidade: 300 , qualidade: '5/10' , cor: 'Pratiado', Uso: 'Manual'},
- //{Nome: 'Chave inglêsa', Quantidade: 300 , qualidade: '3/10' , cor: 'Pratiado', Uso: 'Manual'},
- //{Nome: 'Macaco', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado' , Uso: 'Manual'},
- //{Nome: 'Marreta', Quantidade: 300 , qualidade: '10/10' , cor: 'Pratiado', Uso: 'Manual'}
-//
-//
-//];
-
-//
-//let escolha = prompt('Escolha o uso eletrico ou manual ');
-//
-//let produto = Ferramentas.filter(Ferramentas => Ferramentas.Uso == `${escolha}`);
-//
-//let quadrado = x => x ** 2;
-
-
-//let numeros = [2, -5 , 0,4 , 75 , -3,5];
-//let quadrados = numeros.map(quadrado);
-//let dobros = numeros.map(n => n * 2);
-
-
-//let soma = numeros.reduce((ant, atual) => ant + atual); 
-
-
-
-
-//console.log(produto);
-
-
-//console.log(quadrado);
-//console.log(numeros);
-//console.log(quadrados);
-//console.log(dobros);
-//console.log(soma);
-
-
-let pessoa = {
-    nome: 'Saul',
-    dizerola2: function(){
-        console.log( `Ola meu nome é ${this.nome}`);
+        console.log('Você  é infermero');
 
     }
 
 
-}
+
+    
+    let user = prompt('Seu nome:');
+    //
+    let usarname = Usuario.filter(Usuario => Usuario.nome == `${user}` );
+    
+    
+   let escreveridade = prompt('Seu idade:');
+    
+   let useridade = Usuario.filter(Usuario => Usuario.idade == `${escreveridade}` );
+    
+    let escreveraltura = prompt('Seu Altura:');
+    //
+    //let useraltura = Usuario.filter(Usuario => Usuario.altura == `${escreveraltura}` );
+//
+    //let escreverpeso = prompt('Seu Peso:');
+    //
+    //let useralpeso = Usuario.filter(Usuario => Usuario.peso == `${escreverpeso}` );
+//
+    //let escreveremal = prompt('Seu E-mal:');
+    //
+    //let useremail = Usuario.filter(Usuario => Usuario.email == `${escreveremal}` );
 
 
 
-pessoa.dizerola2();
+
+    
+    console.log(usarname);
+    console.log(useridade);
+
+
+    //console.log();
+    //console.log();
+    //console.log();
