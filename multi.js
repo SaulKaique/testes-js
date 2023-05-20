1530189706c013cffa69b23de43e4c658ed09439
SaulKaique <Saul.silva@etec.sp.gov.br>
2023-05-20 20:20:57 -0300

    agr isso só mostra gifs  eu tenho com uma api

--- multi.js
+++ multi.js
@@ -9,15 +9,34 @@ const exiberinfou = (usuario) => {
 
 
 
-const iniciar = () => {
-    console.log('antes da api')
-    fetch("https://api.github.com/users/SaulKaique")
-    .then((Response) => Response.json())
-    .then((result) => exiberinfou(result))
-    .then(() => console.log('depois da api'));
-    
+  
 
+const iniciar = async() => {
+
+const Response =  await fetch('https://api.github.com/users/SaulKaique')
+console.log(Response);
 
 };
+
+
+    //api para gf
+
+
+
+//http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5//
+
+//S46sECcA2NRLPHKpBJTG2lHDG4Izxb38
+//http://api.giphy.com/v1/gifs/search gif
+
+
+//    console.log('antes da api')
+//    fetch("https://api.github.com/users/SaulKaique")
+//    .then((Response) => Response.json())
+//    .then((result) => exiberinfou(result))
+//    .then(() => console.log('depois da api'));
+//    
+//
+//
+//};
 
 document.addEventListener('DOMContentLoaded' , iniciar);
