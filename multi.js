65d3bf0956266d26f438a3f8ab30813d90493999
SaulKaique <Saul.silva@etec.sp.gov.br>
2023-05-22 16:37:01 -0300

    quase todos os itens feito mas os gifs não esta aparecendo

--- multi.js
+++ multi.js
@@ -1,42 +1,47 @@
-const exiberinfou = (usuario) => {
-    console.log(`O usuario ${usuario.login} possui ${usuario.followers} seguidores`)
+const url = 'https://api.giphy.com/v1/gifs/search';
 
+let busqueda = '?q';
+
+const key = '&api_key=yL1ol1BWPzoaHR2Oc6XIpUpqDqY3aceD';
+const limite = "&limit=40";
+
+let q = "cat";
+
+let urlcompleta = "";
+const btn = document.getElementById('btn');
+
+btn.onclick = () => {
+        q =  document.getElementById('buscar').value;
+
+        urlcompleta = url + busqueda + q +  key +limite;
+    console.log( getdata()) ; 
+
+}
+
+urlcompleta = url + busqueda + q + key +limite ;
+
+const getdata = async () => {
+        await fetch(urlcompleta)
+        .then((response) => {
+     return response.json();
+  })
+        .then((giphy) => {
+ console.log(giphy);
+
+       
+
+      for (let i = 0; i < giphy.data.length; i++) {
+        const gif = document.createElement('img');
+        
+        gif.src = giphy.data(i).images["original"].url;
+        gif.className = "mb-3";
+        document.getElementById("portfolio").appendChild(gif);
+}
+
+})
 
 }
 
 
 
-
-
-
-  
-
-const iniciar = async() => {
-
-const Response =  await fetch('https://api.github.com/users/SaulKaique')
-console.log(Response);
-
-};
-
-
-    //api para gf
-
-
-
-//http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5//
-
-//S46sECcA2NRLPHKpBJTG2lHDG4Izxb38
-//http://api.giphy.com/v1/gifs/search gif
-
-
-//    console.log('antes da api')
-//    fetch("https://api.github.com/users/SaulKaique")
-//    .then((Response) => Response.json())
-//    .then((result) => exiberinfou(result))
-//    .then(() => console.log('depois da api'));
-//    
-//
-//
-//};
-
-document.addEventListener('DOMContentLoaded' , iniciar);
+//"https://media4.giphy.com/media/LPob0bRfqNrFb8Ejrx/giphy.gif?cid=471d4f5cb5l1ushlycp0tdhz9j7x2qn91dvqcotpbeh5xy04&ep=v1_gifs_trending&rid=giphy.gif&ct=g"
