533cab7610513e5fb678c2378dc7dbcc48dac867
SaulKaique <Saul.silva@etec.sp.gov.br>
2023-05-23 15:59:17 -0300

    eu consegui botar a api para funcionar dps de muitos testes

--- multi.js
+++ multi.js
@@ -1,47 +1,66 @@
 const url = 'https://api.giphy.com/v1/gifs/search';
 
-let busqueda = '?q';
+let busqueda = '?q=';
 
-const key = '&api_key=yL1ol1BWPzoaHR2Oc6XIpUpqDqY3aceD';
-const limite = "&limit=40";
+const key = '&api_key=80dSEX3A138U9jkOS9KxafX50StgjIkp';
+const limite = "&limit=1";
 
-let q = "cat";
+let q = "";
 
 let urlcompleta = "";
+
+urlcompleta = url + busqueda + q + key + limite;
+
 const btn = document.getElementById('btn');
+
 
 btn.onclick = () => {
         q =  document.getElementById('buscar').value;
 
         urlcompleta = url + busqueda + q +  key +limite;
-    console.log( getdata()) ; 
+     getdata(); 
+
+     urlcompleta = url + busqueda + q + key +limite ;
+
+        getdata();
 
 }
 
-urlcompleta = url + busqueda + q + key +limite ;
+
 
 const getdata = async () => {
-        await fetch(urlcompleta)
-        .then((response) => {
-     return response.json();
-  })
-        .then((giphy) => {
- console.log(giphy);
 
-       
+       await fetch(urlcompleta).
+       then((response) => {
 
-      for (let i = 0; i < giphy.data.length; i++) {
+        return response.json();
+
+       }).
+       then((giphy) => {
+
+        console.log(giphy)
+
+      
+
+
+
+
+      
+
+ 
+for (let i = 0; i < giphy.data.length; i++) {
         const gif = document.createElement('img');
         
-        gif.src = giphy.data(i).images["original"].url;
+       gif.src = giphy.data[i].images['original'].url;
         gif.className = "mb-3";
         document.getElementById("portfolio").appendChild(gif);
-}
+        console.clear();
+      }
 
 })
+}  
+      getdata();
 
-}
+      
 
 
-
-//"https://media4.giphy.com/media/LPob0bRfqNrFb8Ejrx/giphy.gif?cid=471d4f5cb5l1ushlycp0tdhz9j7x2qn91dvqcotpbeh5xy04&ep=v1_gifs_trending&rid=giphy.gif&ct=g"
